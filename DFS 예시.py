916961aaa45e85e0f505d69aa60b61189f24cecc
이성규 <101550112+PocachipMind@users.noreply.github.com>
2022-03-23 17:19:28 +0900

    Create DFS 예시.py

---
+++ DFS 예시.py
@@ -0,0 +1,23 @@
+# dfs 메소드 정의
+def dfs(graph, v, visited):
+    # 현재 노드 방문 처리
+    visited[v] = True
+    print(v, end=" ")
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+            
+           
+          
+          
+          
+n = 노드 수
+m = 간선 수
+graph = [[] * n]
+          
+for _ in range(m):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+
+visited = [False] * n
