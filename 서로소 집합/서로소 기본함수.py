3ac82c49feb13441d95e10cdc640a65f591e0f43
이성규 <101550112+PocachipMind@users.noreply.github.com>
2022-03-25 14:18:36 +0900

    Create 서로소 기본함수.py

---
+++ 서로소 집합/서로소 기본함수.py
@@ -0,0 +1,23 @@
+# 특정 원소가 속한 집합 찾기
+def find_parent(parent,x):
+    #루트 노드가 아니면 루트노드 찾기
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+#두 원소가 속한 집합 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
+    if a < b:
+        parent[b] = a
+    else :
+        parent[a] = b
+        
+
+v , e = 노드개수, 간선(union 연산)
+parent = [0] * (v + 1) # 부모테이블 초기화
+
+#부모테이블 자기자신을 부모로 초기화
+for i in range(1, v+1):
+    parent[i] = i
