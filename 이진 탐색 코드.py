8e804007c892ef3858b7b6e4417c51858fb7ab53
이성규 <101550112+PocachipMind@users.noreply.github.com>
2022-03-25 10:37:44 +0900

    Update 이진 탐색 코드.py

--- 이진 탐색 코드.py
+++ 이진 탐색 코드.py
@@ -34,3 +34,9 @@ def binary_search_iteration(array, target, start, end):
             start = mid + 1
     #결국 못찾음.
     return None
+
+
+# 파이썬 입력값이 많다면.
+import sys
+# 하나의 문자열 데이터 입력받기
+input_data = sys.stdin.readline().rstrip()
