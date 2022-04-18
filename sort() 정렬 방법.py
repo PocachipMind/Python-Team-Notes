393f4ea31d32f346755c9205ef1182fa2e5648f8
이성규 <101550112+PocachipMind@users.noreply.github.com>
2022-04-18 23:03:22 +0900

    Update sort() 정렬 방법.py

--- sort() 정렬 방법.py
+++ sort() 정렬 방법.py
@@ -22,3 +22,41 @@ students.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
 
 for student in students:
     print(student[0])
+
+    
+
+    # 내림차순은 음수 붙이기!  순서대로 튜플로 쓰기!
+    
+    
+   
+
+
+...
+...
+
+# 또다른 예시
+'''
+[정렬 기준]
+1) 로그의 가장 앞 부분은 식별자다.
+2) 문자로 구성된 로그가 숫자 로그보다 앞에 온다.
+3) 식별자는 순서에 영향을 끼치지 않지만, 문자가 동일할 경우 식별자 순으로 한다.
+4) 숫자 로그는 입력 순서대로 한다.
+'''
+
+# 입력 - logs = ["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]
+# 출력 - [ "let1 art can", "let3 art zero", "let2 own kit dig", "dig1 8 1 5 1" , "dig2 3 6"]
+
+def reorderLogFiles(logs : List[str]) -> List[str]:
+    letters, digits = [], []
+    for log in logs:
+        if log.split()[1].isdigit():
+            digits.append(log)
+        else:
+            letters.append(log)
+    
+    # 2개의 키를 람다 표현식으로 정렬
+    letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+    return letters + digits
+
+
+                
